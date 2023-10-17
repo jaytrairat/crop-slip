96b60be86bbadeea1241e1d310b0504850cc8cc1
jaytrairat <jaytrairat@outlook.com>
2023-10-17 16:08:16 +0700

    feat: support cimb kbank ghb

--- constant/sliptype.go
+++ constant/sliptype.go
@@ -7,4 +7,16 @@ var SLIP_TYPE = map[string]map[string]interface{}{
 		"WIDTH":  1300,
 		"HEIGHT": 400,
 	},
+	"cimb": {
+		"X":      0,
+		"Y":      760,
+		"WIDTH":  720,
+		"HEIGHT": 200,
+	},
+	"kbank": {
+		"X":      135,
+		"Y":      380,
+		"WIDTH":  510,
+		"HEIGHT": 200,
+	},
 }
